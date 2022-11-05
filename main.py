315f3a5bb2b4d57a2e28d440a2bdf0617ea9bc99
Francisco Cezar <franciscocezar.jr@outlook.com>
2022-11-04 22:36:31 -0300

    Placeholder + Centralização da Janela

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from modulos import *
 from frameGrad import GradientFrame
 from reports import Relatorios
 from funcionalidades import Funcs
+from entPlaceHold import EntPlaceHold
 
 root = Tk()
 
@@ -19,13 +20,44 @@ class Application(Funcs, Relatorios):
         self.Menus()
         root.mainloop()
 
+    def center(self, win):
+        # :param win: the main window or Toplevel window to center
+
+        # Apparently a common hack to get the window size. Temporarily hide the
+        # window to avoid update_idletasks() drawing the window in the wrong
+        # position.
+        win.update_idletasks()  # Update "requested size" from geometry manager
+
+        # define window dimensions width and height
+        width = win.winfo_width()
+        frm_width = win.winfo_rootx() - win.winfo_x()
+        win_width = width + 2 * frm_width
+
+        height = win.winfo_height()
+        titlebar_height = win.winfo_rooty() - win.winfo_y()
+        win_height = height + titlebar_height + frm_width
+
+        # Get the window position from the top dynamically as well as position from left or right as follows
+        x = win.winfo_screenwidth() // 2 - win_width // 2
+        y = win.winfo_screenheight() // 2 - win_height // 2
+
+        # this is the line that will center your window
+        win.geometry('{}x{}+{}+{}'.format(width, height, x, y))
+
+        # This seems to draw the window frame immediately, so only call deiconify()
+        # after setting correct window position
+        win.deiconify()
+
     def tela(self):
         self.root.title("Registro de Encomendas")
         self.root.configure(bg='#1e3743')
         self.root.geometry("700x500")
         self.root.resizable(True, True)
         # self.root.maxsize(width=900, height=700)
-        self.root.minsize(width=900, height=700)
+        self.root.attributes('-alpha', 0.0)
+        self.root.minsize(800, 600)
+        self.center(self.root)
+        self.root.attributes('-alpha', 1.0)
 
     def frames_da_tela(self):
         self.frame_1 = GradientFrame(self.root,
@@ -70,22 +102,22 @@ class Application(Funcs, Relatorios):
         self.lb_codigo = Label(self.frame_1, text="Código", bg='#dfe3ee', fg='#107db2')
         self.lb_codigo.place(relx=0.05, rely=0.05)
 
-        self.codigo_entry = Entry(self.frame_1)
+        self.codigo_entry = EntPlaceHold(self.frame_1, 'Digite o Código')
         self.codigo_entry.place(relx=0.05, rely=0.15, relwidth=0.20)
 
         # Label e Entrada Destinatário
 
-        self.lb_destinatario = Label(self.frame_1, text="Destinatário(a)", bg='#dfe3ee', fg='#107db2')
-        self.lb_destinatario.place(relx=0.05, rely=0.35)
+        # self.lb_destinatario = Label(self.frame_1, text="Destinatário(a)", bg='#dfe3ee', fg='#107db2')
+        # self.lb_destinatario.place(relx=0.05, rely=0.35)
 
-        self.destinatario_entry = Entry(self.frame_1)
+        self.destinatario_entry = EntPlaceHold(self.frame_1, 'Digite o nome do Destinatário(a)')
         self.destinatario_entry.place(relx=0.05, rely=0.45, relwidth=0.5)
 
         # Label e Entrada Tipo de Encomenda
         self.lb_tipo = Label(self.frame_1, text="Tipo", bg='#dfe3ee', fg='#107db2')
         self.lb_tipo.place(relx=0.6, rely=0.35)
 
-        self.tipo_entry = Entry(self.frame_1)
+        self.tipo_entry = EntPlaceHold(self.frame_1, 'Digite o Tipo de Encomenda')
         self.tipo_entry.place(relx=0.6, rely=0.45, relwidth=0.30)
 
         # Label e Entrada Funcionário
@@ -157,5 +189,14 @@ class Application(Funcs, Relatorios):
 
         filemenu2.add_command(label="Dados Encomenda", command=self.geraRelatEncomenda)
 
+
+
+
+
+# root.attributes('-alpha', 0.0) # Opcional, para deixar a janela totalmente transparente até os ajustes serem feitos.
+# root.minsize(750, 600)
+# center(root) # A função
+#
+# root.attributes('-alpha', 1.0) # A interface fica visível novamente.
 
 Application()
