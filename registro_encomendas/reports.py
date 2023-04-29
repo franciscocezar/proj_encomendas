d37648da1a8f356fd53bb08f727d594981c5f65d
Francisco Cezar <franciscocezar.jr@outlook.com>
2023-04-29 19:19:21 -0300

    updates and corrections

--- registro_encomendas/reports.py
+++ registro_encomendas/reports.py
@@ -18,38 +18,34 @@ class Relatorios:
 
         self.cursor.execute(f"""SELECT * FROM Quarentena WHERE codigo LIKE '%{codigo}%' """)
         colunas = self.cursor.fetchall()
-        for i in colunas:
-            col0 = i[0]
-            col1 = i[1]
-            col2 = i[2]
-            col3 = i[3]
-            col4 = i[4]
-            col5 = i[5]
-            col6 = i[6]
+
 
         self.c.setFont('Helvetica-Bold', 24)
         self.c.drawString(150, 790, 'Informações da Encomenda')
 
+        etiqueta = ['Código:', 'Destinatário(a):', 'Data de Entrada:', 'Tipo:',
+                    'Funcionário(a):', 'Data de Retirada:', 'Retirado por:']
         self.c.setFont('Helvetica-Bold', 18)
-        self.c.drawString(50, 670, 'Código: ')
-        self.c.drawString(50, 630, 'Destinatário(a): ')
-        self.c.drawString(50, 600, 'Data de Entrada: ')
-        self.c.drawString(50, 570, 'Tipo: ')
-        self.c.drawString(50, 530, 'Funcionário(a): ')
-        self.c.drawString(50, 500, 'Data de Retirada: ')
-        self.c.drawString(50, 470, 'Retirado por: ')
-
-        self.c.setFont('Helvetica-Bold', 24)
-        self.c.drawString(150, 790, 'Informações da Encomenda')
+        count= 670
+        for nome in etiqueta:
+            self.c.drawString(50, count, nome)
+            count -= 40
 
         self.c.setFont('Helvetica', 18)
-        self.c.drawString(130, 670, col0)
-        self.c.drawString(190, 630, col1)
-        self.c.drawString(200, 600, col2)
-        self.c.drawString(100, 570, col3)
-        self.c.drawString(190, 530, col4)
-        self.c.drawString(205, 500, col5)
-        self.c.drawString(170, 470, col6)
+        count= 670
+        for coluna in colunas:
+            for dado in coluna:
+                try:
+                    self.c.drawString(215, count, dado)
+                except AttributeError:
+                    self.c.drawString(215, count, '---')
+                count -= 40
+
+        x1, y1 = 50, 350
+        x2, y2 = 350, 350
+        self.c.setLineWidth(1)
+        self.c.line(x1, y1, x2, y2)
+        self.c.drawString(x1, y1 - 20, "Assinatura")
 
         self.c.showPage()
         self.c.save()
